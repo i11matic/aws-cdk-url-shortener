15f63f2083a02dce1a3ee5c70a03870b3465e94a
Mark Agustin <magustin0329@gmail.com>
2023-02-18 20:37:02 -0800

    fixed line length

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
 
-from aws_cdk_url_shortener.aws_cdk_url_shortener_stack import AwsCdkUrlShortenerStack
+from aws_cdk_url_shortener.aws_cdk_url_shortener_stack import (
+    AwsCdkUrlShortenerStack,
+)
 
 
 app = cdk.App()
-AwsCdkUrlShortenerStack(app, "AwsCdkUrlShortenerStack",
-
-
-    env=cdk.Environment(account='766251705079', region='us-west-2'),
-    )
+AwsCdkUrlShortenerStack(
+    app,
+    "AwsCdkUrlShortenerStack",
+    env=cdk.Environment(account="766251705079", region="us-west-2"),
+)
 app.synth()
